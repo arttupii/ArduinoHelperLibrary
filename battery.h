d5d57a4042b8a97d7f461f845578b80f2a21622b
arttupii <arttupii@gmail.com>
2019-07-05 13:28:40 +0300

    Add files via upload

---
+++ battery.h
@@ -0,0 +1,7 @@
+#ifndef __BATTERY_H___
+#define __BATTERY_H___
+
+//From https://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/?fbclid=IwAR2rj_Ex6ZCzUFRQ4zXcHzJNHLAFfduFPkdcQmunvXy7pMg0hJzo3owTq2g
+long readVcc();
+
+#endif
